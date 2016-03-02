81d515066ec9ea2003bc47337b088bece59018a1
pjang23 <paj48@cornell.edu>
2016-03-01 20:11:32 -0500

    Adding nav.sh

---
+++ lec08/nav.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+for x in {"home","about","work","play"}; do
+    cat base.html | sed 's/\[\[\[\#\#\#\]\]\]/\[\[\['$x'\]\]\]/g' | sed 's/<li><a href="'$x'/<li class="current"><a href="'$x'/g' > "$x.html"
+done
